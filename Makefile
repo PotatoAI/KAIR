facc5ed73020cd590aebe5380fc834b6152d1782
Max Gonzih <gonzih@gmail.com>
2022-09-12 12:06:17 -0700

    Trying to figure out directory structure

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ POETRY = poetry run python
 
 download-all-models:
 	$(POETRY) main_download_pretrained_models.py --models "all"  --model_dir "model_zoo"
+
+process-video:
+	$(POETRY) video.py input.mp4 output.mp4
--- video.py
+++ video.py
@@ -16,22 +16,24 @@ if __name__ == '__main__':
     output_path = sys.argv[2]
     print(f"Converting {input_path} -> {output_path}")
 
+    sh("rm tmp/* -rf")
     in_pic_path = f"tmp/{input_path}"
     os.makedirs(in_pic_path, exist_ok=True)
-    os.makedirs(f"{in_pic_path}/000", exist_ok=True)
-    out_pic_path = f"tmp/{output_path}/000"
+    out_pic_path = f"tmp/{output_path}"
     os.makedirs(out_pic_path, exist_ok=True)
 
-    command = f"ffmpeg -i {input_path} {in_pic_path}/000/%04d.png -hide_banner"
+    command = f"ffmpeg -i {input_path} {in_pic_path}/%04d.png -hide_banner"
     sh(command)
 
     files = glob.glob(f"{in_pic_path}/*.png")
     for fname in files:
         base = os.path.basename(fname)
-        os.makedirs(f"{out_pic_path}/{base}")
+        fdir = f"{in_pic_path}/000"
+        os.makedirs(fdir, exist_ok=True)
+        sh(f"mv {fname} {fdir}/{base}")
 
     task = "001_VRT_videosr_bi_REDS_6frames"
-    command = f"{poetry} main_test_vrt.py --task {task} --folder_lq {in_pic_path} --tile 40 64 64 --tile_overlap 2 20 20"
+    command = f"{poetry} main_test_vrt.py --task {task} --folder_lq {in_pic_path}/000 --tile 40 64 64 --tile_overlap 2 20 20"
     sh(command)
 
     command = f"ffmpeg -framerate 15 -pattern_type glob -i '{out_pic_path}/*.png' -c:v libx264 -pix_fmt yuv420p {output_path}"
