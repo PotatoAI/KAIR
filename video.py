1720cb2c78826cdb106ca769f1abe17e3bca7bab
Max Gonzih <gonzih@gmail.com>
2022-09-12 13:21:37 -0700

    Working script, testing different tasks

--- video.py
+++ video.py
@@ -12,29 +12,39 @@ def sh(cmd: str):
 
 
 if __name__ == '__main__':
+    n = "000"
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     print(f"Converting {input_path} -> {output_path}")
 
-    sh("rm tmp/* -rf")
+    # sh("rm tmp/* -rf")
     in_pic_path = f"tmp/{input_path}"
     os.makedirs(in_pic_path, exist_ok=True)
     out_pic_path = f"tmp/{output_path}"
     os.makedirs(out_pic_path, exist_ok=True)
 
     command = f"ffmpeg -i {input_path} {in_pic_path}/%04d.png -hide_banner"
-    sh(command)
+    # sh(command)
 
     files = glob.glob(f"{in_pic_path}/*.png")
     for fname in files:
         base = os.path.basename(fname)
-        fdir = f"{in_pic_path}/000"
+        fdir = f"{in_pic_path}/{n}"
         os.makedirs(fdir, exist_ok=True)
-        sh(f"mv {fname} {fdir}/{base}")
+        # sh(f"mv {fname} {fdir}/{base}")
 
-    task = "001_VRT_videosr_bi_REDS_6frames"
+    # task = "001_VRT_videosr_bi_REDS_6frames"
+    task = "002_VRT_videosr_bi_REDS_16frames"
+    # task = "003_VRT_videosr_bi_Vimeo_7frames"
+    # task = "004_VRT_videosr_bd_Vimeo_7frames"
+    # task = "005_VRT_videodeblurring_DVD"
+    # task = "006_VRT_videodeblurring_GoPro"
+    # task = "007_VRT_videodeblurring_REDS"
+    # task = "008_VRT_videodenoising_DAVIS"
+
     command = f"{poetry} main_test_vrt.py --task {task} --folder_lq {in_pic_path} --tile 6 128 128 --tile_overlap 2 20 20"
     sh(command)
 
-    command = f"ffmpeg -framerate 15 -pattern_type glob -i '{out_pic_path}/*.png' -c:v libx264 -pix_fmt yuv420p {output_path}"
-    # sh(command)
+    command = f"ffmpeg -framerate 15 -pattern_type glob -i 'results/{task}/{n}/*.png' -c:v libx264 -pix_fmt yuv420p {output_path}"
+    sh(command)
+    sh(f"cp -f output.mp4 outputs/{task}.mp4")
